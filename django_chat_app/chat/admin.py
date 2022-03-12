3477d07ecfd449b916ce510e7ec1fb8c16392f8f
Sebastian Ebel <sebastianebel@hotmail.de>
2022-03-12 07:55:22 +0100

    connection to chat2

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,10 @@
+from .models import Message
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text','created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
