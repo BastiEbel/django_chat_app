b41121410f98203a1161003081ddf348c8a1dc00
Sebastian Ebel <sebastianebel@hotmail.de>
2022-03-13 07:59:01 +0100

    Chats added

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,10 +1,11 @@
-from .models import Message
+from .models import Chat, Message
 from django.contrib import admin
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text','created_at', 'author', 'receiver')
+    fields = ('chat', 'text','created_at', 'author', 'receiver')
     list_display = ('created_at', 'author', 'text', 'receiver')
     search_fields = ('text',)
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
